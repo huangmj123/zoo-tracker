d3c835717effbcf2749f29246fbdc1bc7b6170e6
rhit-huangmj <91560145+huangmj123@users.noreply.github.com>
2025-07-25 17:26:15 -0400

    reset

--- zoo_tracker/lib/init.dart
+++
@@ -1,6 +0,0 @@
-import 'package:sqflite/sqflite.dart';
-
-void init() async{
-var db = await openDatabase('my_db.db');
-  return;
-}
--- zoo_tracker/lib/main.dart
+++ zoo_tracker/lib/main.dart
@@ -33,9 +33,10 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
+  int q = 0;
   var db;
 
-    @override
+  @override
   void initState() {
     super.initState();
   }
@@ -49,8 +50,8 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return FutureBuilder(
-      future: startup(),
-              builder: (context, snapshot) {
+        future: startup(),
+        builder: (context, snapshot) {
           if (snapshot.connectionState == ConnectionState.done) {
             if (snapshot.data == false) {
               print("hi");
@@ -59,52 +60,56 @@ class _MyHomePageState extends State<MyHomePage> {
             }
             return Scaffold(
               appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+                backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+                title: Text(widget.title),
+              ),
+              body: Center(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    const Text(
+                      'You have pushed the button this many times:',
+                    ),
+                    Text('$q'),
+                    Text(
+                      '$_counter',
+                      style: Theme.of(context).textTheme.headlineMedium,
+                    ),
+                  ],
+                ),
+              ),
+              floatingActionButton: FloatingActionButton(
+                onPressed: _incrementCounter,
+                tooltip: 'Increment',
+                child: const Icon(Icons.add),
+              ), // This trailing comma makes auto-formatting nicer for build methods.
             );
           } else {
             // You can show a loading indicator here if needed
             return const SizedBox(
                 height: 200, width: 200, child: CircularProgressIndicator());
-          }});
+          }
+        });
   }
 
   Future<bool> startup() async {
-   try{
-
-    var databasesPath = await getDatabasesPath();
-    String path = '${databasesPath}demo.db';
-    await deleteDatabase(path);
-    db = await openDatabase(path, version: 1,
-      onCreate: (Database db, int version) async {
-      // When creating the db, create the table
-      await db.execute(
-        'CREATE TABLE Test (id INTEGER PRIMARY KEY, name TEXT, value INTEGER, num REAL)');
-    });
-    return true;
-   } catch(e){
-    print(e);
-    return false;
-   }
+    try {
+      q += 1;
+      var databasesPath = await getDatabasesPath();
+      String path = '${databasesPath}demo.db';
+      await deleteDatabase(path);
+      db = await openDatabase(path, version: 1,
+          onCreate: (Database db, int version) async {
+        // When creating the db, create the table
+        await db.execute(
+            'CREATE TABLE Test (id INTEGER PRIMARY KEY, name TEXT, value INTEGER, num REAL)');
+      });
+      print("Hello");
+      q += 8;
+      return true;
+    } catch (e) {
+      print(e);
+      return false;
+    }
   }
 }
